d42a0185a355593cce0c4cf7ead3bd7788266ed5
ezeilo-su <ezeilo.su@gmail.com>
2021-11-26 23:31:01 +0100

    Add endpoint for our API

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,17 @@
 import express, { json, urlencoded } from 'express';
-// import { json, urlencoded } from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(json());
+app.use(morgan('tiny'));
 app.use(urlencoded({ extended: true }));
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.json({
+    status: 'success',
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
